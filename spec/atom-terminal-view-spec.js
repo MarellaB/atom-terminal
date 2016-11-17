8b319157be10f566fac807b2c89416e0e6df9266
MarellaB <brandonmarella@gmail.com>
2016-11-17 10:28:47 -0500

    Copied from github

---
+++ spec/atom-terminal-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTerminalView from '../lib/atom-terminal-view';
+
+describe('AtomTerminalView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
